5321b18af5de39f2e9b0dc7f046b04374b9472f0
nkr <nkgithubnarkuziev108@gmail.com>
2022-06-04 03:49:24 +0500

    updated files

--- src/main.rs
+++ src/main.rs
@@ -2,61 +2,34 @@
 
 // PACKAGES
 use std::fs;
-use thiserror::Error;
-use native_dialog::{FileDialog, MessageDialog, MessageType};
+use std::fs::metadata;
 
+extern crate bytesize;
+use bytesize::ByteSize;
 
-#[derive(Error, Debug)]
-pub enum ErrorBox {
-	#[error("system error or I/O failure")]
-	IoFailure(#[from] std::io::Error),
+// MODULES
+mod dialog;
+pub use crate::dialog::dialog_fn;
 
-	#[error("the implementation returns malformed strings")]
-	InvalidString(#[from] std::string::FromUtf8Error),
-
-	#[error("failed to parse the string returned from implementation")]
-	UnexpectedOutput(&'static str),
-
-	#[error("cannot find any dialog implementation (kdialog/zenity)")]
-	NoImplementation,
-
-	#[error("the implementation reports error")]
-	ImplementationError(String),
-}
-
-type FnResult = Result<String, ErrorBox>;
-fn folder_dialog() -> FnResult {
-	let path = FileDialog::new()
-		.set_location("~/Desktop")
-		.show_open_single_dir()
-		.unwrap();
-
-	let path = match path {
-		Some(path) => path,
-		None => return Err(ErrorBox::NoImplementation),
-	};
-
-	let yes = MessageDialog::new()
-		.set_type(MessageType::Info)
-		.set_title("Do you want to open the file?")
-		.set_text(&format!("{:#?}", path))
-		.show_confirm()
-		.unwrap();
-
-	let newpath = path.into_os_string().into_string().unwrap();
-
-	Ok(newpath)
-}
 
 fn folder_files(folder_path: &str) {
 	let paths = fs::read_dir(folder_path).unwrap();
 
 	for path in paths {
-		println!("Name: {}", path.unwrap().path().display())
+		let md = metadata(path.as_ref().unwrap().path()).unwrap();
+
+		let f_type = match fs::metadata(path.as_ref().unwrap().path()) {
+			Ok(m) => m,
+			Err(e) => panic!("There was a problem opening the file: {:?}", e),
+		};
+
+		println!("Name: {:?}, Dir: {}", path.as_ref().unwrap().file_name(), md.is_dir());
+		//println!("Type: {:?}\n", f_type);
+		println!("{:?}\n", ByteSize::kb(md.len()));
 	}
 }
 
 fn main() {
-	let x = folder_dialog().unwrap();
+	let x = dialog_fn::folder_dialog().unwrap();
 	folder_files(&x);
 }
