7c36a4de553d5ce480966d4903cf5bdee7762dee
nkr <nkgithubnarkuziev108@gmail.com>
2022-06-04 01:47:25 +0500

    updaded files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 #![allow(warnings)]
 
+// PACKAGES
+use std::fs;
+use thiserror::Error;
 use native_dialog::{FileDialog, MessageDialog, MessageType};
 
-fn main() {
+
+#[derive(Error, Debug)]
+pub enum ErrorBox {
+	#[error("system error or I/O failure")]
+	IoFailure(#[from] std::io::Error),
+
+	#[error("the implementation returns malformed strings")]
+	InvalidString(#[from] std::string::FromUtf8Error),
+
+	#[error("failed to parse the string returned from implementation")]
+	UnexpectedOutput(&'static str),
+
+	#[error("cannot find any dialog implementation (kdialog/zenity)")]
+	NoImplementation,
+
+	#[error("the implementation reports error")]
+	ImplementationError(String),
+}
+
+type FnResult = Result<String, ErrorBox>;
+fn folder_dialog() -> FnResult {
 	let path = FileDialog::new()
 		.set_location("~/Desktop")
 		.show_open_single_dir()
@@ -10,7 +33,7 @@ fn main() {
 
 	let path = match path {
 		Some(path) => path,
-		None => return,
+		None => return Err(ErrorBox::NoImplementation),
 	};
 
 	let yes = MessageDialog::new()
@@ -20,5 +43,20 @@ fn main() {
 		.show_confirm()
 		.unwrap();
 
-	println!("{:?}", path);
+	let newpath = path.into_os_string().into_string().unwrap();
+
+	Ok(newpath)
+}
+
+fn folder_files(folder_path: &str) {
+	let paths = fs::read_dir(folder_path).unwrap();
+
+	for path in paths {
+		println!("Name: {}", path.unwrap().path().display())
+	}
+}
+
+fn main() {
+	let x = folder_dialog().unwrap();
+	folder_files(&x);
 }
